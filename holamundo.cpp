491d17203bb57d2e356a559077f52b1e30abd0fb
kellyvasquez <kefervaza.19@hotmail.com>
2017-02-11 17:12:00 -0500

    metodos printf y scanf

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main (){
-    printf ("Hola Mundo");
-    
+    printf ("Hola Mundo Cotecnova 2017");
+    int edad;
+    printf("\nIngrese su edad\n");
+    scanf("%d", &edad);
+    printf("su edad es %d", edad);
     return 0;
 
     }
